f63bcf2fd94a458bf04aab75b98f34b8034122ce
rui-saito <rui_saito@mail.toyota.co.jp>
2023-12-31 01:30:57 +0900

    sample code writing

--- __test__/Component1.test.tsx
+++ __test__/Component1.test.tsx
@@ -1,7 +1,19 @@
 import Component1 from "../src/Component1";
 import { render, screen } from "@testing-library/react-native";
 
-it("test", async () => {
-  render(<Component1 />);
-  expect(screen.getByText("This is Component!")).toBeTruthy();
+describe("Component1", () => {
+  it("This is Component!というテキストが見える", async () => {
+    render(<Component1 />);
+    expect(screen.getByText("This is Component!")).toBeDefined();
+  });
+
+  it("インプットボックス見える", async () => {
+    render(<Component1 />);
+    expect(screen.getByTestId("sampleTextBox")).toBeDefined();
+  });
+
+  it("ボタンというテキストのあるボタンが見える", async () => {
+    render(<Component1 />);
+    expect(screen.getByText("ボタン")).toBeDefined();
+  });
 });
--- src/Component1.tsx
+++ src/Component1.tsx
@@ -1,5 +1,61 @@
-import { Text } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Component1() {
-  return <Text>This is Component!</Text>;
+  return (
+    <View style={styles.brock}>
+      <Text style={styles.TitleText}>This is Component!</Text>
+      <TextInput
+        style={styles.input}
+        testID="sampleTextBox"
+        defaultValue="テキストボックス"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          alert("aaa");
+        }}
+      >
+        <Text style={styles.buttonText}>ボタン</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  brock: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "pink",
+    width: "100%",
+  },
+  TitleText: {
+    justifyContent: "center",
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  input: {
+    borderStyle: "solid",
+    width: "80%",
+    borderWidth: 1,
+    marginTop: 10,
+    padding: 8,
+  },
+});
